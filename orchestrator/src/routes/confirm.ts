e19390f2116b620c0710ac98b23adda363a5501f
Yannick Utard <yannickutard@gmail.com>
2018-06-07 11:33:33 +0200

    orch: typo

--- orchestrator/src/routes/confirm.ts
+++ orchestrator/src/routes/confirm.ts
@@ -2,7 +2,7 @@ import * as express from 'express';
 
 import { Server } from '../models/server';
 
-export default async function connect(req: express.Request, res: express.Response) {
+export default async function confirm(req: express.Request, res: express.Response) {
   const { serverId } = req.params;
 
   const server = await Server.findById(serverId).exec();
